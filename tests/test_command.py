af5c91004789cb17df8d5179a5eb53180ac0ddd2
suzzy777 <suzzanarafi@gmail.com>
2022-06-20 14:24:30 +0000

    Fix flaky test

--- tests/test_command.py
+++ tests/test_command.py
@@ -59,6 +59,8 @@ class TestCommands:
 
     @responses.activate
     def test_command_list(self, centreon_con):
+        wsresponses = '{"authToken": "NTc1MDU3MGE3M2JiODIuMjA4OTA2OTc="}'
+        responses.add(responses.POST, 'http://api.domain.tld/centreon/api/index.php?action=authenticate', body=wsresponses, status=200, content_type='application/json')
         with open(resource_dir / 'test_commands_list.json') as data:
             wsresponses = json.load(data)
         responses.add(responses.POST,
